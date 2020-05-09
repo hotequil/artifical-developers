543624a530752f986780bc0301ac42b0b0b52dfe
hotequil <joaohotequil@gmail.com>
2020-05-09 11:25:53 -0300

    update in menu.

---
+++ src/javascripts/menu-control.js
@@ -0,0 +1,9 @@
+$('a[href^="#"]').on('click', function(e) {
+    e.preventDefault();
+
+    let id = $(this).attr('href'), targetOffset = $(id).offset().top;
+
+    $('html, body').animate({
+        scrollTop: targetOffset - 50
+    }, 750);
+});
