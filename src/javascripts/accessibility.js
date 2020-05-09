c3a680adf4398631dae461ab52b59c399de3d5cd
hotequil <joaohotequil@gmail.com>
2020-05-09 18:17:32 -0300

    corrigido problemas de navegação com keyboard

--- src/javascripts/accessibility.js
+++ src/javascripts/accessibility.js
@@ -1,17 +1,17 @@
 $('.accessibility-bar__button').on('click keydown', function(e){
-    e.preventDefault();
-
     if((e.type === "keydown" && e.keyCode === 13) || e.type === "click") {
+        e.preventDefault();
+
         let body = $('body');
         let bodyFontSize = $('body').css("fontSize");
 
-        if(e.currentTarget.innerHTML === 'A+'){
+        if(e.currentTarget.innerHTML.includes('A+')){
             if(bodyFontSize === '16px' || bodyFontSize === ''){
                 body.css('fontSize', '17px');
             } else if(bodyFontSize === '17px'){
                 body.css('fontSize', '18px');
             };
-        } else if(e.currentTarget.innerHTML === 'A-'){
+        } else if(e.currentTarget.innerHTML.includes('A-')){
             if(bodyFontSize === '18px'){
                 body.css('fontSize', '17px');
             } else if(bodyFontSize === '17px'){
